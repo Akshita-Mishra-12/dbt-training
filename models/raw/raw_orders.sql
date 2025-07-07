c12c5af59cdce860a893a54d46d673a76f279642
akshi.study.material@gmail.com <akshi.study.material@gmail.com>
2025-07-07 13:56:07 +0000

    develpoed raw_orders, raw_customer, raw_product

---
+++ models/raw/raw_orders.sql
@@ -0,0 +1,11 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+-- above is a configuration block, type __ , it will list all dbt functions, select config and it will now change creation of default view to table.
+-- dbt run will execute all models present in models folder. 
+-- dbt run --select raw will execute only mentioned model or folder
+
+select * 
+from RAW.GLOBALMART.ORDERS
